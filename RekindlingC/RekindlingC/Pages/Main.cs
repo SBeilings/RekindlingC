6f27b434c4f06309ce4dd718fa39e7743d14105d
Shamiel Beilings <104254915+SBeilings@users.noreply.github.com>
2024-08-04 04:47:26 +0100

    Add files via upload

---
+++ RekindlingC/RekindlingC/Pages/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RekindlingC.Pages
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            RussianDolls russianDolls = new RussianDolls();
+            russianDolls.RunMePlease();
+        }
+
+    }
+}
---
+++ RekindlingC/RekindlingC/Pages/RussianDolls.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace RekindlingC.Pages
+{
+  public class RussianDolls
+    {
+        public void RunMePlease() {
+
+            FirstRussianDoll firstRussianDoll = new FirstRussianDoll();
+            firstRussianDoll.callNext();
+        }
+    }
+     class FirstRussianDoll
+    {
+
+        public void callNext()
+        {
+
+            SecondRussianDOll secondRussianDOll = new SecondRussianDOll();
+            secondRussianDOll.RunMe();
+        }
+    }
+
+}
+
+ class SecondRussianDOll
+{
+
+    public void RunMe()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Hello World! \nIs anyone there or did I hear enough?");
+            string answer = Console.ReadLine();
+            string name;
+
+            if (answer == "y" || answer == "yes")
+            {
+                Console.Write("Nice to have you with us, what is your name?");
+                name = Console.ReadLine();
+                Console.WriteLine(name + " is a lovely name, my Creator's name is Shamiel" +
+                    "\n Well I have got to get going now, See ya!");
+            }
+            else if (answer == "enough" || answer == "Enough")
+            {
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Humans would have answered with a y or a yes!\n So sad to be all alone ");
+            }
+        }
+        
+    }
+}
+
